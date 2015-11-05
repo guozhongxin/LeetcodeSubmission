5cc66b53e73040ab082752ed2775695468fb1e43
guozhongxin <buptmr.guo@gmail.com>
2015-11-05 22:19:16 +0800

    D

---
+++ src/LongestPalindromicSubstring.java
@@ -0,0 +1,66 @@
+/**
+ * Created by guozhongxin on 15/9/23.
+ */
+public class LongestPalindromicSubstring {
+	public static String longestPalindrome_1(String s) {
+
+		int length = s.length();
+
+		int longest = 0;
+		String ll = "";
+		for (int i = 0; i<length-1; i++){
+			for (int j = i+1; j<length ; j++){
+				String subString = s.substring(i,j+1);
+				int l = judgePalindrome(subString);
+				longest = (longest>l)?longest:l;
+				ll=(longest>l)?ll:subString;
+				System.out.print("");
+			}
+		}
+
+		return ll;
+	}
+
+	public static int judgePalindrome(String s) {
+		int length = s.length();
+		int halfLength = Math.round(length / 2);
+		for (int i = 0; i<halfLength; i++){
+			if (s.charAt(i) != s.charAt(length-1-i)){
+				return 0;
+			}
+		}
+		return length;
+	}
+
+	public static String longestPalindrome(String s){
+		int length = s.length();
+		int longest = 1;
+		String longestPalindrome = s.substring(0, 1);
+
+		for (int i =0; i<= 2*length-1 ; i++){
+			int end = i/2+1;
+			int start = i-end;
+//			int bound = (start>(length-1-end))?start:(length-1-end);
+			while ( start>= 0 && end<length ){
+				if (s.charAt(start)==s.charAt(end)){
+					int l = end-start+1;
+					if(longest < l){
+						longest = l;
+						longestPalindrome = s.substring(start,end+1);
+					}
+				}else{
+					break;
+				}
+				--start;
+				++end;
+			}
+		}
+
+		return longestPalindrome;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(longestPalindrome("aabaaaaa"));
+//		System.out.println("01234".substring(4,5));
+	}
+}
---
+++ src/ZigZagConversion.java
@@ -0,0 +1,113 @@
+/**
+ * Created by guozhongxin on 15/9/24.
+ */
+public class ZigZagConversion {
+
+	public static String convert_1(String s, int numRows) {
+
+		int length = s.length();
+		if (numRows==1){
+			return s;
+		}
+		int column = (length/(numRows*2-2)+1)*2;
+		char[][] zz = new char[numRows][column];
+		int[] iRow = new int[numRows];
+		for (int i =0; i<numRows;i++){
+			iRow[i]=0;
+		}
+
+		for (int i = 0; i<length; i++){
+			int remainder = i%(2*numRows-2);
+			int row=0;
+			if(remainder<numRows) {
+				row = remainder;
+			}else {
+				row = 2*numRows-remainder-2;
+			}
+			zz[row][iRow[row]]=s.charAt(i);
+			iRow[row]+=1;
+		}
+
+		String zzs = "";
+		for (int i =0; i<numRows;i++){
+			for (int j = 0; j<iRow[i];j++){
+				zzs+=zz[i][j];
+			}
+		}
+		return zzs;
+	}
+
+	public static String convert_2(String s, int numRows) {
+
+		int length = s.length();
+		if (numRows==1){
+			return s;
+		}
+		int column = (length/(numRows*2-2)+1)*2;
+		char[][] zz = new char[numRows][column];
+		int[] iRow = new int[numRows];
+		for (int i =0; i<numRows;i++){
+			iRow[i]=0;
+		}
+
+		for (int i = 0; i<length; i++){
+			int remainder = i%(2*numRows-2);
+			int row= numRows-1-Math.abs(remainder-numRows+1);
+//			if(remainder<numRows) {
+//				row = remainder;
+//			}else {
+//				row = 2*numRows-remainder-2;
+//			}
+			zz[row][iRow[row]]=s.charAt(i);
+			iRow[row]+=1;
+		}
+
+		String zzs = "";
+		for (int i =0; i<numRows;i++){
+			zzs+= new String(zz[i],0,iRow[i]);
+			for (int j = 0; j<iRow[i];j++){
+//				zzs+=zz[i][j];
+			}
+		}
+		return zzs;
+	}
+
+	public static String convert(String s, int numRows) {
+
+		int length = s.length();
+		if (numRows==1){
+			return s;
+		}
+//		int column = (length/(numRows*2-2)+1)*2;
+		String[] zz = new String[numRows];
+//		ArrayList<ArrayList<Character>> zz = new ArrayList<ArrayList<Character>>();
+//		int[] iRow = new int[numRows];
+//		for (int i =0; i<numRows;i++){
+//			iRow[i]=0;
+//		}
+
+		for (int i = 0; i<length; i++){
+			int remainder = i%(2*numRows-2);
+			int row= numRows-1-Math.abs(remainder-numRows+1);
+//			if(remainder<numRows) {
+//				row = remainder;
+//			}else {
+//				row = 2*numRows-remainder-2;
+//			}
+			zz[row]+=s.charAt(i);
+		}
+
+		String zzs = "";
+		int min = numRows>length?length:numRows;
+		for (int i =0; i<min;i++){
+			zzs+= new String(zz[i]);
+		}
+		return zzs.substring(4);
+	}
+
+
+	public static void main(String[] args) {
+		String s = "sdgfjsfj1";
+		System.out.println(convert(s,3));
+	}
+}
